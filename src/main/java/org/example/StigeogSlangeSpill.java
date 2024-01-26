3ba95d5cc4f7115091ba6d65c5e2354198a3d72b
h572659 <jonasgrundvaagrong@gmail.com>
2024-01-26 08:28:08 +0100

    Ryddet opp. Flyttet logikk i main metoden til hjelpemetoder -Jonas

--- src/main/java/org/example/StigeogSlangeSpill.java
+++ src/main/java/org/example/StigeogSlangeSpill.java
@@ -11,10 +11,38 @@ public class StigeogSlangeSpill {
         Scanner scanInt = new Scanner(System.in);
         Scanner scan = new Scanner(System.in);
         Brett nyttSpill = new Brett();
-        boolean gyldigSpill = false;
         Spiller[] spillere;
 
+        // Velg antall spillere
+        antall = antallSpillere(scanInt);
+
+        spillere = new Spiller[antall];
+
+        //Velg navn på spillere
+        velgNavn(spillere, scan);
+
+        // Gjennbruker antall variablet for a telle runder
+        antall = 1;
+        System.out.println("The game begins!");
+
+        // Dette er selve game loopen, while brukes for å holde spillet i gang, for loopen brukes for å velge spilleren
+        // som skal spille. sleep metoden brukes for å "simulere" selve kastet på terningen.
+
+        spillLoop(nyttSpill, spillere, scan, antall);
+
+        scan.close();
+        scanInt.close();
+    }
+
+    /**
+     * Dette er en hjelpe metode brukt i oppstart av spill
+     * @param scanInt Scanner som brukes for å velge antall spillere
+     * @return returnerer antall spillere i spillet
+     */
+    private static int antallSpillere(Scanner scanInt) {
         // While lokken er brukt for å sikre at brukere gir et antall spillere for de fortsetter.
+        boolean gyldigSpill = false;
+        int antall = 0;
         while (!gyldigSpill) {
             System.out.println("How many players do you want? Min 2, Maxs 4");
             antall = scanInt.nextInt();
@@ -23,20 +51,33 @@ public class StigeogSlangeSpill {
             }
             gyldigSpill = antall >= 2 && 4 >= antall;
         }
+            return antall;
+    }
+
+    /**
+     * Denne metoden brukes for å få navn av bruker
+     * @param spillere dette er deltagerene
+     * @param scan denne scanner classen brukes for å skrive inn navn
+     */
+    private static void velgNavn(Spiller[] spillere, Scanner scan) {
         // Etter at antall spillere er registrert, må brukere gi navn. Vi gjør dette gjennom en for lokke
-        spillere = new Spiller[antall];
         for (int i = 0; i < spillere.length; i++) {
             System.out.println("Name of the player: " + (i+1));
             String navn = scan.nextLine();
             spillere[i] = new Spiller(navn);
             System.out.println("Hope you have fun, " + spillere[i].getName() + "!");
         }
-        // Gjennbruker antall variablet for a telle runder
-        antall = 1;
-        System.out.println("The game begins!");
-        // Dette er selve game loopen, while brukes for å holde spillet i gang, for loopen brukes for å velge spilleren
-        // som skal spille. sleep metoden brukes for å "simulere" selve kastet på terningen.
-        while (!nyttSpill.foundWinner()) {
+    }
+
+    /**
+     * Dette er selve spill loopen, det er her spillet foregår
+     * @param spill dette er brettet spillet tar plass på
+     * @param spillere spillerene som deltar
+     * @param scan Scanner metode brukes når bruker "triller"
+     * @param antall Denne variabelen brukes for å ha kontroll på runder
+     */
+    private static void spillLoop(Brett spill, Spiller[] spillere, Scanner scan, int antall) {
+        while (!spill.foundWinner()) {
             System.out.println("*****************");
             System.out.println("Round " + antall);
             System.out.println("*****************");
@@ -60,15 +101,13 @@ public class StigeogSlangeSpill {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                nyttSpill.terningKast(spiller);
-                if (nyttSpill.foundWinner()) {
+                spill.terningKast(spiller);
+                if (spill.foundWinner()) {
                     break;
                 }
                 System.out.println();
             }
             antall++;
         }
-        scan.close();
-        scanInt.close();
     }
 }
